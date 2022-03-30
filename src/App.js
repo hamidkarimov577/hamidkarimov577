6a09add0df4a5490e4457830ab0d5f515ebf7f8f
hamidkarimov577 <hamidkarimov577@gmail.com>
2022-03-30 23:34:36 +0500

    qoshldi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import {Book} from './Book'
-import './App.css';
+import { Book } from "./Book"
+import "./App.css"
 
 export default function App() {
-  return ( 
-  <div>
-    <Book name= 'JS for beginners' year= '2022' price='50'></Book>
-    Extra inform
-    <Book name= 'React for beginners' year= '2021' price='60' />
-    <Book name= 'Vue for beginners' year= '2024' price='70' />
-  </div>)
-  
+  return (
+    <div>
+      <Book name="JS for beginners" year="2022" price="50"></Book>
+      Extra inform
+      <Book name="React for beginners" year="2021" price="60" />
+      <Book name="Vue for beginners" year="2024" price="70" />
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
 }
-
-
